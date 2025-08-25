3891c1fecc1ae001c1ad9d59b3899ebc60dd46ab
Ian Synge <gamersynge@gmail.com>
2025-08-25 15:22:39 -0400

    Updated app.py and models.py

--- server/app.py
+++ server/app.py
@@ -14,8 +14,50 @@ class ClearSession(Resource):
         session['user_id'] = None
 
         return {}, 204
+    
+class Signup(Resource):
+    def post(self):
+        json = request.get_json()
+        user = User(username=json.get('username'))
+        user.password_hash = json.get('password')
+        db.session.add(user)
+        db.session.commit()
+        session['user_id'] = user.id
+        return UserSchema().dump(user), 201
+    
+class Login(Resource):
+    def post(self):
+        username = request.get_json()['username']
+        password = request.get_json()['password']
+
+        user = User.query.filter(User.username == username).first()
+
+        if user and user.authenticate(password):
+            session['user_id'] = user.id
+            return UserSchema().dump(user), 200
+
+        return {'error': '401 Unauthorized'}, 401
+    
+class Logout(Resource):
+    def delete(self):
+        session['user_id'] = None
+        return {}, 204
+    
+class CheckSession(Resource):
+    def get(self):
+        user_id = session.get('user_id')
+
+        if user_id:
+            user = User.query.get(user_id)
+            return UserSchema().dump(user), 200
+
+        return {}, 204
 
 api.add_resource(ClearSession, '/clear', endpoint='clear')
+api.add_resource(Signup, '/signup')
+api.add_resource(Login, '/login')
+api.add_resource(Logout, '/logout')
+api.add_resource(CheckSession, '/check_session')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
